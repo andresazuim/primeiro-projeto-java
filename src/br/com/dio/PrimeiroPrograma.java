0003b3db17f87a8bd7a420053d04da947ce41d02
andresazuim <andresazuim37@gmail.com>
2022-07-27 23:56:01 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		
+
+		
+		System.out.println(gato);
+
+
+		
+	/*	int a = 2;
+		int b = 3;
+		System.out.println("Heloo World!" + (a+b));*/
+	}
+
+
+}
+ 
